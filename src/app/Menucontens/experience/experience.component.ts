2fd71678938723e6ea27565c941724d266e4a33b
Sanso <sowmisanso07@gmail.com>
2024-12-04 15:01:30 +0530

    fourth push

--- src/app/Menucontens/experience/experience.component.ts
+++ src/app/Menucontens/experience/experience.component.ts
@@ -17,10 +17,10 @@ export class ExperienceComponent {
 
  // Experience entries
  experience1 = {
-   year:'OCT 2022-OCT 2024',
+   year:'NOV 2022-OCT 2024',
    company: 'CKS Solutions, Nagercoil',
-   title: 'FullStack Developer',
-   description: 'Designed and coded new software or modified existing software to add new features. Developed mobile applications using Flutter, ensuring seamless performance across different platforms. Collaborated with cross-functional teams to ensure high-quality application development. Conducted testing and debugging to optimize mobile applications.'
+   title: 'Junior Software Developer',
+   description: 'Specializes in crafting dynamic and user-friendly web pages that elevate the digital experience. Expertise includes developing frontend applications using Angular and optimizing APIs with .NET and Entity Framework to deliver seamless backend solutions. Focuses on enhancing existing software with new features and collaborating with cross-functional teams to ensure high-quality, efficient applications. Passionate about problem-solving, with a strong emphasis on rigorous testing and debugging to ensure robust and reliable results.'
  };
 
  experience2 = {
@@ -38,10 +38,10 @@ export class ExperienceComponent {
  };
 
  experience4 = {
-   year:'DEC 2020-OCT 2022',
+   year:'OCT 2021-SEP 2022',
    company: 'Beta Monks Technology Pvt Ltd, Chennai',
-   title: 'Junior IT Support',
-   description: 'Skilled in user authentication, 3D Secure (E-Commerce) ACS applications, and OTP page design/customization. Experienced in bug detection, SQL database management, and web applications within the banking domain.'
+   title: 'Junior Software Developer',
+   description: 'Proficient in managing user authentication and 3D Secure (E-Commerce) applications, with hands-on expertise in designing and customizing OTP pages. Experienced in identifying and resolving bugs, managing SQL databases, and optimizing web applications within the banking domain. Demonstrated ability to deliver reliable solutions that enhance system performance and user experience.'
  };
  downloadCV() {
    const link = document.createElement('a');
